c2feb069e67c8feba777ce347e383fe4420a6c9a
celong00 <michaelalexanderrustan@gmail.com>
2022-09-19 15:31:46 +0700

    Initial commit

---
+++ src/main/kotlin/com/example/springboot/kotlinspringboot/KotlinSpringBootApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springboot.kotlinspringboot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringBootApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringBootApplication>(*args)
+}
